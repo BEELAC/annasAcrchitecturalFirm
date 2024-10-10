1d1f5c7aeff375fd1b317e595b790c16a4d808ee
BEELAC <cdegeorge96gmai@ip-172-31-72-243.ec2.internal>
2024-10-10 18:57:04 +0000

    Added home page with link cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importing components
+import { HomeComponent } from './features/home/home.component';
+import { ClientMeetingComponent } from './features/client-meeting/client-meeting.component';
+import { ClientSchedulingComponent } from './features/client-scheduling/client-scheduling.component';
 
+// Defining routes
+const routes: Routes = [
+  {path: '', component: HomeComponent}, // Default route pointing to HomeComponent
+  {path: 'client-meeting', component: ClientMeetingComponent}, // Route for client meetings
+  {path: 'client-scheduling', component: ClientSchedulingComponent}, // Route for client scheduling
+  {path: '**', redirectTo: ''} // This redirects any unkown paths to the home page
+];
+
+// Configuring RouterModule with the routes
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] // Export RouterModule to make it available throughout your app
 })
 export class AppRoutingModule { }
---
+++ src/app/features/client-scheduling/client-scheduling.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-scheduling',
+  templateUrl: './client-scheduling.component.html',
+  styleUrls: ['./client-scheduling.component.css']
+})
+export class ClientSchedulingComponent {
+
+}
